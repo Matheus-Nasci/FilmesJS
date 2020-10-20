e17056a96514ef2be0dbd1740850a4d9ef14ea29
Matheus Nascimento De Oliveira <matheus.nasci2002@hotmail.com>
2020-10-20 00:39:26 -0300

    Javascript

--- Banco de dados/Scripts/senai-filmes-02-DML.sql
+++ Banco de dados/Scripts/senai-filmes-02-DML.sql
@@ -5,19 +5,17 @@ GO
 -- Insere dois gêneros na tabela Generos
 INSERT INTO Generos	(Nome)
 VALUES				('Ação')
-					,('Drama')
-					,('Romance');
+					,('Drama');
 GO
 
 -- Insere dois filmes na tabela Filmes
 INSERT INTO Filmes	(Titulo, IdGenero)
 VALUES				('A vida é bela', 2)
-					,('Rambo', 1)
-					,('Sei lá', 3);
+					,('Rambo', 1);
 GO
 
 -- Insere dois novos usuários
 INSERT INTO Usuarios (Nome,Email, Senha, Permissao)
 VALUES				 ('Saulo','saulo@email.com', '123', 'Comum')
-					,('Administrador','adm@adm.com', '123', 'Administrador');
+					,('Helena','adm@adm.com', '123', 'Administrador');
 GO
